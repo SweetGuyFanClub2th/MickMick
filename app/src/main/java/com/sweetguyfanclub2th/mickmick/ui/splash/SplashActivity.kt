1fbc41c16b5c5828f6b7d831969ba8c28dfffbf7
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-02 14:47:51 +0900

    Feat : `SplashActivity.kt` Splash View Complete
    - 스플래쉬 화면 띄우기 완료
    - Sharedpreference를 이용한 viewpager 확인이 추후 필요.

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/splash/SplashActivity.kt
@@ -1,15 +1,43 @@
 package com.sweetguyfanclub2th.mickmick.ui.splash
 
+import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sweetguyfanclub2th.mickmick.databinding.ActivitySplashBinding
+import android.os.Handler
+import android.os.Looper
+import com.sweetguyfanclub2th.mickmick.R
+import com.sweetguyfanclub2th.mickmick.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_splash)
+
+        var SharedPreferences = getSharedPreferences("welcomeViewWatched", Activity.MODE_PRIVATE)
+        var checkSharedPreferences = SharedPreferences.getBoolean("welcomeViewWatched", false)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(Runnable {
+            when(checkSharedPreferences){
+                false -> moveWelcomeAndEditSharedPref(SharedPreferences)
+                else -> moveLoginPage()
+            }
+        }, 3000)
+    }
+
+    private fun moveWelcomeAndEditSharedPref(SharedPreferences : SharedPreferences){
+        val editBoolean = SharedPreferences.edit()
+        editBoolean.putBoolean("welcomeViewWatched", true)
+        editBoolean.apply()
+        startActivity(Intent(this@SplashActivity, WelcomeActivity::class.java))
+    }
+
+    private fun moveLoginPage(){
+        Intent(this, LoginActivity::class.java).apply {
+            startActivity(this)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/splash/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.sweetguyfanclub2th.mickmick.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sweetguyfanclub2th.mickmick.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
