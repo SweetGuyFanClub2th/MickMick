fee62847de744bd1d13e1682fbe7aecdb3e5f021
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-04 23:55:29 +0900

    Chore : `WelcomeAdapter` File name edit
    - 제곧내

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/splash/intro/WelcomeActivity.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/splash/intro/WelcomeActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.sweetguyfanclub2th.mickmick.databinding.ActivityWelcomeBinding
 import com.sweetguyfanclub2th.mickmick.ui.login.LoginActivity
