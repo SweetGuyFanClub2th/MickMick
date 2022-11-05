5ad1522076fc2315fa66a62c078ec28cb8c80a14
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-05 13:55:07 +0900

    Chore : `build.gradle` implementation
    - splash 제거
    - coroutine implementation
    - splash 제거에 따른 여러 내용들 변경

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/login/LoginActivity.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/login/LoginActivity.kt
@@ -8,7 +8,6 @@ import android.text.Editable
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.sweetguyfanclub2th.mickmick.databinding.ActivityLoginBinding
@@ -37,10 +36,10 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    override fun onStart(){
-        super.onStart()
-        moveMainPage(auth?.currentUser)
-    }
+//    override fun onStart(){
+//        super.onStart()
+//        moveMainPage(auth?.currentUser)
+//    }
 
     private fun loginCompleteCheck(emailText: Editable, passwdText: Editable): Boolean {
         val emailLength = emailText.length
