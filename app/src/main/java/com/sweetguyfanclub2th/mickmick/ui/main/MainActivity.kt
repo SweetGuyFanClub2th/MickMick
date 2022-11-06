35c548c238a4c9b5dd0630c5ea78d30d2c57100a
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-06 17:06:30 +0900

    Feat : `MainActivity.kt` Get Hash Key
    - 해쉬키값 로그로 볼 수 있음

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/MainActivity.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.sweetguyfanclub2th.mickmick.ui.main
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import com.sweetguyfanclub2th.mickmick.databinding.ActivityMainBinding
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getAppKeyHash()
 
 //        supportFragmentManager.beginTransaction().add(R.id.fragment, MainFragment()).commit()
 //
@@ -26,5 +32,22 @@ class MainActivity : AppCompatActivity() {
 //
 //            true
 //        }
+    }
+
+    // 앱 해시 키 얻는 코드
+    fun getAppKeyHash() {
+        try {
+            val info =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                var md: MessageDigest
+                md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val something = String(Base64.encode(md.digest(), 0))
+                Log.e("Hash key", something)
+            }
+        } catch (e: Exception) {
+            Log.e("name not found", e.toString())
+        }
     }
 }
