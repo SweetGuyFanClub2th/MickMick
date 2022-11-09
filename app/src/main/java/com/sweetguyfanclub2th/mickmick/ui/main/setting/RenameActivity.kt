1ef9ba1b9d3b6cf03733faf670d087d2c25c3c47
Haram Lee <74528522+iamraram@users.noreply.github.com>
2022-11-09 12:20:14 +0900

    feat: `SettingActivity` add function, change Nickname
    닉네임 DB도 같이 바꿔야 하고, 이름도 추가해야 함

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/setting/RenameActivity.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/setting/RenameActivity.kt
@@ -2,7 +2,9 @@ package com.sweetguyfanclub2th.mickmick.ui.main.setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
@@ -10,10 +12,11 @@ import com.sweetguyfanclub2th.mickmick.databinding.ActivityRenameBinding
 
 class RenameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRenameBinding
-    private var auth: FirebaseAuth? = null
-    private lateinit var db: FirebaseFirestore
-    private lateinit var beforeName: String
-    private lateinit var beforeNickname: String
+
+    private lateinit var bfNickname: String
+    private lateinit var bfName: String
+
+    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +26,67 @@ class RenameActivity : AppCompatActivity() {
         binding.backpress.setOnClickListener {
             finish()
         }
-        var database = FirebaseFirestore.getInstance()
 
-        val firebaseUser = FirebaseAuth.getInstance().currentUser
+        val emailFind = FirebaseAuth.getInstance()
+        val firebaseUser = emailFind.currentUser
+
+        binding.registerEmail.isEnabled = false
+
         if (firebaseUser != null) {
-            binding.registerEmail.hint = firebaseUser.email.toString()
+            val email = firebaseUser.email.toString()
+            binding.registerEmail.hint = email
+            val userInfo = db.collection(email).document("userinfo")
+
+            userInfo.get().addOnSuccessListener {
+                val nickname = it.get("nickname").toString()
+                binding.registerNickname.text =
+                    Editable.Factory.getInstance().newEditable(nickname)
+                bfNickname = binding.registerNickname.text.toString()
+            }
+//            userInfo.get().addOnSuccessListener {
+//                val name = it.get("name").toString()
+//                binding.registerName.text =
+//                    Editable.Factory.getInstance().newEditable(name)
+//            }
         }
         else {
             binding.registerEmail.hint = "유저 정보가 잘못되었습니다."
         }
 
         binding.editComplete.setOnClickListener {
+            if (binding.registerName.text.toString() == "") {
+                editInfo(
+                    "", binding.registerNickname.text.toString()
+                )
+            }
+            else if (binding.registerNickname.text.toString() == "") {
+                Toast.makeText(this, "닉네임은 반드시 입력해야 합니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                editInfo(
+                    binding.registerName.text.toString(),
+                    binding.registerNickname.text.toString()
+                )
+            }
+        }
+    }
 
+    private fun editInfo(name: String, nickName: String) {
+        try {
+            val email = FirebaseAuth.getInstance().currentUser?.email.toString()
+            val editUser = db.collection(email).document("userinfo")
+//            val beForeNickname = db.collection("nickname").document("names")
+
+//            beForeNickname.update("bfNickname", nickName)
+            editUser.update("nickname", nickName)
+
+            Toast.makeText(this, "닉네임이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        catch (nullPointerException: NullPointerException) {
+            Toast.makeText(this, "유저 정보 불러오기에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            Log.d("TAG", "editInfo: NullPointerException")
+            finish()
         }
     }
 }
