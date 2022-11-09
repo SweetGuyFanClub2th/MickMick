4352c7776b630f0aca37861f89a30a5da3f289f3
Haram Lee <74528522+iamraram@users.noreply.github.com>
2022-11-09 10:45:38 +0900

    chore: `SettingFragment` get userEmail in editText

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/setting/RenameActivity.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/setting/RenameActivity.kt
@@ -2,10 +2,18 @@ package com.sweetguyfanclub2th.mickmick.ui.main.setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import com.sweetguyfanclub2th.mickmick.databinding.ActivityRenameBinding
 
 class RenameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRenameBinding
+    private var auth: FirebaseAuth? = null
+    private lateinit var db: FirebaseFirestore
+    private lateinit var beforeName: String
+    private lateinit var beforeNickname: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +22,19 @@ class RenameActivity : AppCompatActivity() {
 
         binding.backpress.setOnClickListener {
             finish()
+        }
+        var database = FirebaseFirestore.getInstance()
+
+        val firebaseUser = FirebaseAuth.getInstance().currentUser
+        if (firebaseUser != null) {
+            binding.registerEmail.hint = firebaseUser.email.toString()
+        }
+        else {
+            binding.registerEmail.hint = "유저 정보가 잘못되었습니다."
+        }
+
+        binding.editComplete.setOnClickListener {
+
         }
     }
 }
