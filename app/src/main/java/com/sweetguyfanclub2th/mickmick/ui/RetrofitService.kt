767ea939d67e620cb82a9183e3d2e6d673ffae93
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-11-10 17:46:38 +0900

    Feat : `MapFragment` Get Poi Data Complete
    - 데이터 가져오기까지 완료

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/RetrofitService.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/RetrofitService.kt
@@ -5,18 +5,20 @@ import com.sweetguyfanclub2th.mickmick.data.searchpois.PoisResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RetrofitService {
     @GET("tmap/pois")
     fun getPois(
-        @Query("version") version: Int = 1,
-        @Query("searchKeyword") keyword: String,
-        @Header("Authorization") authToken: String
+        @Query("version", encoded = true) version: Int,
+        @Query("searchKeyword", encoded = false) keyword: String,
+        @Header("appKey") authToken: String
     ): Call<PoisResponse>
     
     @GET("puzzle/congestion/rltm/pois/{poiId}")
     fun getCongestion(
-        @Header("Authorization") authToken: String
+        @Header("appKey") authToken: String,
+        @Path("poiId") poiId : String
     ): Call<CongestionResponse>
 }
--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/map/MapFragment.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/map/MapFragment.kt
@@ -1,30 +1,36 @@
 package com.sweetguyfanclub2th.mickmick.ui.main.map
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.skt.tmap.TMapView
+import com.sweetguyfanclub2th.mickmick.data.searchpois.PoisResponse
 import com.sweetguyfanclub2th.mickmick.databinding.FragmentMapBinding
+import com.sweetguyfanclub2th.mickmick.ui.RetrofitClient
+import com.sweetguyfanclub2th.mickmick.ui.RetrofitService
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class MapFragment : Fragment() {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
+    private val authToken = "l7xx7de642979fac440f8fad597ef2584f9e"
     companion object {
         private val retrofitClient: MapFragment = MapFragment()
 
         fun getInstance(): MapFragment {
             return retrofitClient
         }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
@@ -39,7 +45,8 @@ class MapFragment : Fragment() {
             }
 
             else{
-
+                val keyword = binding.mapSearch.text.toString()
+                getDataToRetrofit(keyword)
             }
         }
 
@@ -54,7 +61,28 @@ class MapFragment : Fragment() {
         super.onStart()
 
         val tmapview = TMapView(requireContext())
-        tmapview.setSKTMapApiKey("l7xx7de642979fac440f8fad597ef2584f9e")
+        tmapview.setSKTMapApiKey(authToken)
         binding.mapView.addView(tmapview)
+    }
+
+    private fun getDataToRetrofit(keyword : String){
+        val retrofit = RetrofitClient.getApiService().getPois(1, keyword, authToken)
+
+        retrofit.enqueue(object : Callback<PoisResponse>{
+            override fun onFailure(call: Call<PoisResponse>, t: Throwable) {
+                Log.d(TAG, t.message!!)
+            }
+
+            override fun onResponse(
+                call: Call<PoisResponse>,
+                response: Response<PoisResponse>
+            ) {
+                Log.d(TAG, "response : ${response.body()?.searchPoiInfo}") // 정상출력
+
+                Log.d(TAG, "response (errorBody) : ${response.errorBody()}")
+                Log.d(TAG, "response (message) : ${response.message()}")
+                Log.d(TAG, "response (code) : ${response.code()}")
+            }
+        })
     }
 }
