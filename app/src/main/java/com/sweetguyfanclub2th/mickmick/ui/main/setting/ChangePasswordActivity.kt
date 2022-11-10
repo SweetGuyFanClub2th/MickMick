c9a502f86b62bca383bbb6c10ba36532114fb762
Haram Lee <74528522+iamraram@users.noreply.github.com>
2022-11-10 23:04:09 +0900

    feat: `SettingFragment` add auth, sendPasswordResetEmail

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/setting/ChangePasswordActivity.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/setting/ChangePasswordActivity.kt
@@ -1,10 +1,12 @@
 package com.sweetguyfanclub2th.mickmick.ui.main.setting
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 import com.sweetguyfanclub2th.mickmick.R
 import com.sweetguyfanclub2th.mickmick.databinding.ActivityChangePasswordBinding
+import com.sweetguyfanclub2th.mickmick.ui.splash.SplashActivity
 
 
 class ChangePasswordActivity : AppCompatActivity() {
@@ -28,6 +30,12 @@ class ChangePasswordActivity : AppCompatActivity() {
         val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.frame, ChangePasswordFragment2())
         transaction.commit()
+    }
+
+    fun gotoSplash() {
+        val intent = Intent(this, SplashActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
 }
--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/setting/ChangePasswordFragment2.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/setting/ChangePasswordFragment2.kt
@@ -1,22 +1,46 @@
 package com.sweetguyfanclub2th.mickmick.ui.main.setting
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.sweetguyfanclub2th.mickmick.databinding.FragmentChangePassword2Binding
 
 class ChangePasswordFragment2 : Fragment() {
     private var _binding: FragmentChangePassword2Binding? = null
     private val binding get() = _binding!!
+    private val email = FirebaseAuth.getInstance().currentUser?.email.toString()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentChangePassword2Binding.inflate(inflater, container, false)
+        binding.passwordFirstCheck.text =
+            Editable.Factory.getInstance().newEditable(email)
+        binding.editComplete.setOnClickListener {
+            send(binding.passwordFirstCheck.text.toString())
+        }
 
         return binding.root
+    }
+
+    private fun send(em: String) {
+        val auth = Firebase.auth
+
+        auth.sendPasswordResetEmail(em)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(activity, "이메일을 보냈습니다. 다시 로그인해주세요.", Toast.LENGTH_SHORT).show()
+                    auth.signOut()
+                    (activity as ChangePasswordActivity).gotoSplash()
+                }
+            }
     }
 }
