47a02144f0808561067ddd3fc18e7f9417cd7053
Taeseo Moon / 문태서 <98258284+ansxotj06@users.noreply.github.com>
2022-11-10 12:23:05 +0900

    fix: merge error

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/todo/TodoFragment.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/todo/TodoFragment.kt
@@ -1,90 +1,65 @@
-package com.sweetguyfanclub2th.mickmick.ui.main.todo.menu
+package com.sweetguyfanclub2th.mickmick.ui.main.todo
 
-import android.content.ContentValues
 import android.os.Bundle
-import android.provider.SyncStateContract.Helpers.update
-import android.text.Editable
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
-import com.sweetguyfanclub2th.mickmick.R
-import com.sweetguyfanclub2th.mickmick.databinding.FragmentScheduleBinding
+import com.sweetguyfanclub2th.mickmick.databinding.FragmentTodoBinding
+import com.sweetguyfanclub2th.mickmick.ui.main.map.MapFragment
+import com.sweetguyfanclub2th.mickmick.ui.main.todo.menu.ScheduleFragment
 
 
-class ScheduleFragment : Fragment() {
-    private var _binding: FragmentScheduleBinding? = null
+class TodoFragment : Fragment() {
+    private var _binding: FragmentTodoBinding? = null
     private val binding get() = _binding!!
 
-    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()  // Firestore 인스턴스 선언
-    private var email = FirebaseAuth.getInstance().currentUser?.email.toString()
-    private lateinit var nickname: String
+    private var auth: FirebaseAuth? = null
+    private lateinit var db: FirebaseFirestore
 
+    private val tabTitleArray = arrayOf("Schedule", "Map")
 
-//    private lateinit var database: DatabaseReference
-//    database = Firebase.database.reference
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentTodoBinding.inflate(inflater, container, false)
 
+        //
 
-        _binding = FragmentScheduleBinding.inflate(inflater, container, false)
-
-        binding.addTodoBtn.setOnClickListener() {
-            var todo = binding.addTodoEdit.text.toString();  // 버튼 클릭시 입력한 텍스트를 문자열로 todo에 저장
-            todoDataSetUpload(todo, email);
-
-        }
-
-
-        return binding.root
+        val view = binding.root
+        return view
     }
 
-    private fun updateData(){
-        var todo = binding.addTodoEdit.text.toString();  // 버튼 클릭시 입력한 텍스트를 문자열로 todo에 저장
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        var map= mutableMapOf<String,Any>()
-        map["todo"] = todo.toString();
+        val viewPagerAdapter = TodoViewPagerAdapter(requireActivity())
+        viewPagerAdapter.addFragment(MapFragment())
+        viewPagerAdapter.addFragment(ScheduleFragment())
 
+        binding.todoViewPager.adapter = viewPagerAdapter
+        binding.todoViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
+        })
+
+        TabLayoutMediator(binding.tabLayout, binding.todoViewPager) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
     }
 
-
-    private fun todoDataSetUpload(todo: String, email : String){
-
-        updateData()
-
-        fun updateData(){
-            val todo = binding.addTodoEdit.text.toString();  // 버튼 클릭시 입력한 텍스트를 문자열로 todo에 저장
-            val email = FirebaseAuth.getInstance().currentUser?.email.toString()  // 사용자의 email을 firebase에서 불러와 저장함
-
-            var map= mutableMapOf<String,Any>()
-            map["todo"] = todo.toString();
-
-            getEmail(email)
-
-            db.collection(email).document("todo").update(map)  // email을 기준으로 컬렉션을 고르고, 투두 문서에 내용을 저장?
-                .addOnCompleteListener {
-                    if (it.isSuccessful){
-                        // 프래그먼트 새로고침
-                    }
-                }
-        }
-
-    }
-
-    private fun getEmail(email: String) {
-        val todoRef = db.collection(email).document("names")
-        todoRef.get().addOnSuccessListener {
-            nickname = it.get("nickname").toString()
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
