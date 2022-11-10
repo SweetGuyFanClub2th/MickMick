166e55529f48aa0f24a1574e707bc97bb07eee81
Haram Lee <74528522+iamraram@users.noreply.github.com>
2022-11-11 02:40:40 +0900

    feat: `HomeFragment` add current date

--- app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/sweetguyfanclub2th/mickmick/ui/main/home/HomeFragment.kt
@@ -1,22 +1,42 @@
 package com.sweetguyfanclub2th.mickmick.ui.main.home
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
 import com.sweetguyfanclub2th.mickmick.R
-
+import com.sweetguyfanclub2th.mickmick.databinding.ActivityRenameBinding
+import com.sweetguyfanclub2th.mickmick.databinding.FragmentHomeBinding
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class HomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
+    @SuppressLint("NewApi", "SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val current = LocalDateTime.now()
+        val time = current.format((DateTimeFormatter.ofPattern("yyyyMMdd")))
+        val showingTime = current.format(DateTimeFormatter.ofPattern("MM월 dd일"))
+
+        Log.d("time", showingTime)
+        binding.nowDate.text = "$showingTime, "
+
+        // TODO: 유저 todo 목록에서 앞 연도, 월, 일이 현재 연도, 월, 일과 같은 것만 뽑아서 보여주기
+
+        return binding.root
     }
 }
